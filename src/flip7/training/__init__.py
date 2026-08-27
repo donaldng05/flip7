705f299e8070fca34e0af733ecdd8fdf116de19a
donaldng05 <quyduongnguyen05@gmail.com>
2026-08-26 23:45:42 -0700

    refactor: add pluggable opponent lineups and per-seat observations

--- src/flip7/training/__init__.py
+++ src/flip7/training/__init__.py
@@ -1,6 +1,15 @@
 """Training orchestration for learned policies."""
 
+from flip7.training.league import (
+    LeagueConfig,
+    LeaguePPOTrainer,
+    PolicyLeague,
+    PolicySnapshot,
+    write_population,
+)
 from flip7.training.ppo import (
+    EpisodeLineup,
+    OpponentProvider,
     PPOConfig,
     PPOTrainer,
     Rollout,
@@ -13,7 +22,14 @@ __all__ = [
     "PPOConfig",
     "PPOTrainer",
     "Rollout",
+    "EpisodeLineup",
+    "OpponentProvider",
+    "LeagueConfig",
+    "LeaguePPOTrainer",
+    "PolicyLeague",
+    "PolicySnapshot",
     "baseline_factories",
     "compute_gae",
     "write_history",
+    "write_population",
 ]
